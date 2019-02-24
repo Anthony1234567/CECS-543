03e57bb465c7fa7a15d5baeca0104a79c2a22c22
Sotheanith Sok <thetheanith@gmail.com>
2019-02-23 18:59:33 -0800

    Update index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const Manifest= require('./src/js/manifest');
-
-let manifest=new Manifest('./manifest');
-manifest.createEntry(1,[123,15,213]);
-manifest.addArtifactsToEntry(2,[1,2,3,4,5,6,7,8,9])
-//manifest.removeArtifactsFromEntry(2,[2,4]);
